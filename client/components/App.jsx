378e57c04fc62616f10ddd6521dfdc6b9fb41fc1
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-02 23:42:34 -0600

    change view on navbar click

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,45 +6,34 @@ import ImagePage from './ImagePage/ImagePage';
 import NavBar from './NavigationBar';
 import ImageUpload from './ImageUpload/ImageUpload';
 
-// function changeView({ view }) {
-//   if (view === 'Homepage') {
-//     return <Homepage />;
-//   }  if (view === 'ImageUpload') {
-//     return <ImageUpload />;
-//   }  if (view === 'ImagePage') {
-//     return <ImagePage />;
-//   }  if (!view) {
-//     return <Homepage />;
-//   }
-// }
-//          <div id= 'main-view'className='main-view'>{changeView(view)}"new view"</div>
-
-function mainView() {
-  const view = useState({view})
-  return <div> view ? <h2>view</h2> : 'no view'</div>
+function MainView({ view }) {
+  return view === 'ImageUpload' ? (
+    <ImageUpload />
+  ) : view === 'ImagePage' ? (
+    <ImagePage />
+  ) : (
+    <HomePage />
+  );
 }
 
 export default function App() {
   const [isShowingSidebar, setIsShowingSidebar] = useState(true);
   const [view, setView] = useState('HomePage');
+
   return (
     <div id="app-container" className="app-container">
       <LogIn />
 
       <div id="content-row" className="content-row">
         <>
-          {isShowingSidebar && <NavBar />}
+          {isShowingSidebar && <NavBar setView={setView} view={view} />}
           <div id="main-area" className="main-area">
             <button onClick={() => setIsShowingSidebar(!isShowingSidebar)}>
               Toggle Sidebar Here
             </button>
-            <div>
-              <div>
-                {view}
-              </div>
-              <HomePage />
-              <ImageUpload />
-              <ImagePage />
+
+            <div id="main-view" className="main-view">
+              <MainView view={view} />
             </div>
           </div>
         </>
--- client/components/NavigationBar.jsx
+++ client/components/NavigationBar.jsx
@@ -1,41 +1,29 @@
-import { useState } from 'react';
-
-export default function NavBar() {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [view, setView] = useState('HomePage');
-
+export default function NavBar({ view, setView }) {
   return (
     <div id="sidebar" className="sidebar">
       <div id="navigation-bar" className="navigation-bar">
         <nav>
-          <button onClick={() => setIsExpanded(!isExpanded)}>
-            Menu
-            <span className={`indicator ${isExpanded ? 'down' : 'right'}`}>
-              &#9650;
-            </span>
-          </button>
-
-          {isExpanded && (
-            <ul>
-              <li>
-                <button
-                  onClick={() => {
-                    setView('HomePage');
-                  }}
-                >
-                 {'Go to Home'}
-                </button>
-              </li>
-              <li>
-                <button
-                  onClick={() => {
-                    setView('ImageUpload');
-                  }}
-                >{'Go to Upload'}</button>
-              </li>
-              <li>{view}</li>
-            </ul>
-          )}
+          <ul>
+            <li>
+              <button
+                onClick={() => {
+                  setView('HomePage');
+                }}
+              >
+                {'Go to Home'}
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  setView('ImageUpload');
+                }}
+              >
+                {'Go to Upload'}
+              </button>
+            </li>
+            <li>{view}</li>
+          </ul>
         </nav>
       </div>
     </div>
